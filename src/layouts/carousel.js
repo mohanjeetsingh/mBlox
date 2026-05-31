f1fb21410d39b4969941a7c238dcb36d66d3ebc7
mohanjeetsingh <mohanjeet.singh@gmail.com>
2026-06-01 00:48:27 +0530

    cover pb removed

--- src/layouts/carousel.js
+++ src/layouts/carousel.js
@@ -1,15 +1,16 @@
-import { RESPONSIVE_CAROUSEL_CLASSES_M3E } from '../core/config.js';
+import { RESPONSIVE_CAROUSEL_CLASSES_M3E, BLOCK_COVER } from '../core/config.js';
 
 export function renderCarouselGrid(renderedBlocks, config) {
     if (!renderedBlocks || renderedBlocks.length === 0) return '';
-    
+
     const autoColsClass = RESPONSIVE_CAROUSEL_CLASSES_M3E[config.columnCount] || RESPONSIVE_CAROUSEL_CLASSES_M3E[6];
     const rowClassMap = { 1: 'grid-rows-1', 2: 'grid-rows-2', 3: 'grid-rows-3', 4: 'grid-rows-4' };
     const rowClass = rowClassMap[config.blockRows] || 'grid-rows-1';
-    
-    let html = `<div class="grid grid-flow-col ${rowClass} ${autoColsClass} overflow-x-auto snap-x snap-mandatory scroll-smooth gap-4 pb-8 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">`;
+    const pbClass = config.blockType === BLOCK_COVER ? '' : ' pb-8';
+
+    let html = `<div class="grid grid-flow-col ${rowClass} ${autoColsClass} overflow-x-auto snap-x snap-mandatory scroll-smooth gap-4 ${pbClass} [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">`;
     html += renderedBlocks.join('');
     html += `</div>`;
-    
+
     return html;
 }
