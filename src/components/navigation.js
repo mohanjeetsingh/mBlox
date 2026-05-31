0f8b42fdf9b2ae5c1422fc3883023d63b91ce74d
mohanjeetsingh <mohanjeet.singh@gmail.com>
2026-05-31 22:25:58 +0530

    visual improvement

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,13 +1,13 @@
 export function renderCarouselControls(config) {
-    const baseClass = `absolute top-0 bottom-0 z-10 flex items-center justify-center w-16 cursor-pointer opacity-70 hover:opacity-100 transition-opacity ${config.theme.text} rounded-full hover:bg-tertiary hover:text-on-tertiary`;
-    const prevClass = config.containsNavigation ? "nav-prev" : "js-carousel-prev pb-5";
-    const nextClass = config.containsNavigation ? "nav-next" : "js-carousel-next pb-5";
+  const baseClass = `absolute top-0 bottom-0 z-10 flex items-center justify-center w-16 cursor-pointer opacity-70 hover:opacity-100 transition-opacity ${config.theme.text} hover:backdrop-blur-xl`;
+  const prevClass = config.containsNavigation ? "nav-prev" : "js-carousel-prev pb-5";
+  const nextClass = config.containsNavigation ? "nav-next" : "js-carousel-next pb-5";
 
-    const prev = `<button class="${baseClass} left-0 ${prevClass}" type="button" title="Click for Previous" aria-label="Previous">
+  const prev = `<button class="${baseClass} left-0 ${prevClass}" type="button" title="Click for Previous" aria-label="Previous">
             <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><use href="#icon-caret-left"></use></svg>
           </button>`;
-    const next = `<button class="${baseClass} right-0 ${nextClass}" title="Click for Next" type="button" aria-label="Next">
+  const next = `<button class="${baseClass} right-0 ${nextClass}" title="Click for Next" type="button" aria-label="Next">
             <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><use href="#icon-caret-right"></use></svg>
           </button>`;
-    return { prev, next };
+  return { prev, next };
 }
