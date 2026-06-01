88bd86ce517e2c2156ba559d831f3d778913c993
mohanjeetsingh <mohanjeet.singh@gmail.com>
2026-06-01 10:18:21 +0530

    height fixed

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,7 +1,10 @@
+import { BLOCK_COVER } from '../core/config.js';
+
 export function renderCarouselControls(config) {
-  const baseClass = `absolute top-0 bottom-0 z-10 flex items-center justify-center w-16 cursor-pointer opacity-70 hover:opacity-100 transition-opacity ${config.theme.text} hover:backdrop-blur-xl`;
-  const prevClass = config.containsNavigation ? "nav-prev" : "js-carousel-prev pb-5";
-  const nextClass = config.containsNavigation ? "nav-next" : "js-carousel-next pb-5";
+  const bottomInset = config.blockType === BLOCK_COVER ? "bottom-0" : "bottom-8";
+  const baseClass = `absolute top-0 ${bottomInset} z-10 flex items-center justify-center w-16 cursor-pointer opacity-70 hover:opacity-100 transition-opacity ${config.theme.text} hover:backdrop-blur-xl`;
+  const prevClass = config.containsNavigation ? "nav-prev" : "js-carousel-prev";
+  const nextClass = config.containsNavigation ? "nav-next" : "js-carousel-next";
 
   const prev = `<button class="${baseClass} left-0 ${prevClass}" type="button" title="Click for Previous" aria-label="Previous">
             <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><use href="#icon-caret-left"></use></svg>
