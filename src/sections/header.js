ad7d0757070ea7f24f1caf229019960066807b50
mohanjeetsingh <mohanjeet.singh@gmail.com>
2026-06-01 15:55:19 +0530

    bg neutralised

--- src/sections/header.js
+++ src/sections/header.js
@@ -3,5 +3,5 @@ import { renderHeaderDescription } from '../components/header-description.js';
 
 export function renderBlockHeader(config) {
     if (!config.dataTitle) return '';
-    return `<div class="text-center ${config.theme.bg} py-8">${renderHeaderTitle(config)}${renderHeaderDescription(config)}</div>`;
+    return `<div class="p-8 @lg:p-12">${renderHeaderTitle(config)}${renderHeaderDescription(config)}</div>`;
 }
